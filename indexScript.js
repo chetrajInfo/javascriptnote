ca3fd3793b38819344ae856bf7ffc3f8cb817e6b
Chet Raj Bhandari <chetrajbhandari001@gmail.com>
2023-04-19 18:51:04 -0500

    added javascript event Handler

---
+++ indexScript.js
@@ -0,0 +1,9 @@
+
+var canButton = document.querySelector("#cancel");
+canButton.addEventListener('click',()=> {
+     document.getElementById("fullname").value="";
+     document.getElementById("email").value="";
+     document.getElementById("pass").value="";
+   
+      
+});
