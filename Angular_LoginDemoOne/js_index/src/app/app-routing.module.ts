abbd670b0a9488510550edfe9c0a72c92a7ed314
myinfo <chetraj0101@gmail.com>
2023-04-22 17:52:54 -0500

    added the routing to open anothe component with dropdown menu

--- Angular_LoginDemoOne/js_index/src/app/app-routing.module.ts
+++ Angular_LoginDemoOne/js_index/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { IndexbodyComponent } from './indexbody/indexbody.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', redirectTo: '/loginpage', pathMatch: 'full' },
+  { path: 'loginpage', component: LoginpageComponent },
+  { path: 'indexbody', component: IndexbodyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
